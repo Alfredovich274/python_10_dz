ac3b66e0b80c9cb938ae3d3b630d8a7ea536a614
Pavel Kirillov <68268029+Alfredovich274@users.noreply.github.com>
2022-07-01 18:49:59 +0300

    дз 10

--- 1.py
+++
@@ -1,7 +0,0 @@
-def newfunc(n):
-     def myfunc(x):
-         return x + n
-     return myfunc
-
-new = newfunc(100)  # new - это функция
-print(new(200))
--- myfunctions.py
+++ myfunctions.py
@@ -163,10 +163,10 @@ def my_filter(iterable, function):
     :param function: функция фильтрации
     :return: новая отфильтрованная последовательность
     """
-    pass
+    return list(filter(function, iterable))
 
 
-# print(my_filter([1, 2, 3, 4, 5], lambda x: x > 3) == [4, 5])  # True
-# print(my_filter([1, 2, 3, 4, 5], lambda x: x == 2) == [2])  # True
-# print(my_filter([1, 2, 3, 4, 5], lambda x: x != 3) == [1, 2, 4, 5])  # True
-# print(my_filter(['a', 'b', 'c', 'd'], lambda x: x in 'abba') == ['a', 'b'])  # True
+print(my_filter([1, 2, 3, 4, 5], lambda x: x > 3) == [4, 5])  # True
+print(my_filter([1, 2, 3, 4, 5], lambda x: x == 2) == [2])  # True
+print(my_filter([1, 2, 3, 4, 5], lambda x: x != 3) == [1, 2, 4, 5])  # True
+print(my_filter(['a', 'b', 'c', 'd'], lambda x: x in 'abba') == ['a', 'b'])  # True
